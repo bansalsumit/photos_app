23201c68ef08869e299d2949498b7f4b743ee637
sumit bansal <sumit.bansal@loconav.com>
2020-04-30 19:00:55 +0530

    Gallery component initial structure created

---
+++ src/containers/Gallery/Gallery.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux/Aux';
+
+class Gallery extends Component {
+    render () {
+        return (
+            <Aux></Aux>
+        );
+    }
+}
+
+export default Gallery;
